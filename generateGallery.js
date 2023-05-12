1d3df68fd217a2488353a2b196c8d563c62848c8
Jason Elisei <1eliseijas@hdsb.ca>
2023-05-12 00:34:37 -0400

    Add description popup

--- generateGallery.js
+++ generateGallery.js
@@ -1,9 +1,37 @@
 const imgs='https://raw.githubusercontent.com/MTN73/landscape-gallery/main/imgs.json';
 
-let urls;
+const rowHeight = 300;
+
+let scroll;
+window.addEventListener("scroll", (event) => {
+    scroll = this.scrollY;
+});
+
+let info;
     fetch(imgs)
     .then(response => response.json())
     .then(data => {
-        urls = data;
-        console.log(urls);
+        info = data;
+
+        //arrange in random order
+
+        let count = 0;
+        for (const description of info["DESCRIPTIONS"]){
+            let card = document.createElement("div");
+            card.id = "card"+count;
+            let img = document.createElement("img");
+            img.id = "img"+count;
+            //only if is seen by user
+            img.src = info["URLS"][count];
+            card.appendChild(img);
+            
+            let desc = document.createElement("p");
+            desc.id = "desc"+count;
+            let descContent = document.createTextNode(description);
+            desc.appendChild(descContent);
+            card.appendChild(desc);
+
+            document.getElementById("container").appendChild(card);
+            count++;
+        }
     });
